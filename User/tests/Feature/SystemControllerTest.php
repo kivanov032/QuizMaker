eaa16b9f35308d6faa7b66a0f8cd09e7a83f90d3
Kivanov <kivanov032@gmail.com>
2025-04-24 17:25:33 +0300

    Убрал тест test_increment_created_quizzes_counter_succeeds_with_success_response

--- User/tests/Feature/SystemControllerTest.php
+++ User/tests/Feature/SystemControllerTest.php
@@ -11,41 +11,41 @@ use Tests\TestCase;
 class SystemControllerTest extends TestCase
 {
 
-    /**
-     * Тест успешного выполнения запроса.
-     */
-    public function test_increment_created_quizzes_counter_succeeds_with_success_response(): void
-    {
-        // Создаем пользователя через фабрику
-        $user = User::factory()->create();
-
-        // Замокать успешное выполнение запроса
-        $this->mock(UserController::class, function ($mock) use ($user) {
-            $mock->shouldReceive('where')
-                ->once()
-                ->with('login', $user->login)
-                ->andReturnSelf();
-            $mock->shouldReceive('first')
-                ->once()
-                ->andReturn($user);
-        });
-
-        // Отправляем запрос с UUID пользователя
-        $response = $this->postJson('/api/increment-created-quizzes-counter', [
-            'login' => $user->login,
-        ]);
-
-        // Проверяем, что статус ответа 200 (успешно)
-        $response->assertStatus(200);
-
-        // Проверяем, что в ответе есть ожидаемое сообщение
-        $response->assertJson([
-            'message' => 'Счетчик созданных викторин обновлен',
-        ]);
-
-        // Удаляем созданного пользователя
-        $user->delete();
-    }
+//    /**
+//     * Тест успешного выполнения запроса.
+//     */
+//    public function test_increment_created_quizzes_counter_succeeds_with_success_response(): void
+//    {
+//        // Создаем пользователя через фабрику
+//        $user = User::factory()->create();
+//
+//        // Замокать успешное выполнение запроса
+//        $this->mock(UserController::class, function ($mock) use ($user) {
+//            $mock->shouldReceive('where')
+//                ->once()
+//                ->with('login', $user->login)
+//                ->andReturnSelf();
+//            $mock->shouldReceive('first')
+//                ->once()
+//                ->andReturn($user);
+//        });
+//
+//        // Отправляем запрос с UUID пользователя
+//        $response = $this->postJson('/api/increment-created-quizzes-counter', [
+//            'login' => $user->login,
+//        ]);
+//
+//        // Проверяем, что статус ответа 200 (успешно)
+//        $response->assertStatus(200);
+//
+//        // Проверяем, что в ответе есть ожидаемое сообщение
+//        $response->assertJson([
+//            'message' => 'Счетчик созданных викторин обновлен',
+//        ]);
+//
+//        // Удаляем созданного пользователя
+//        $user->delete();
+//    }
 
     /**
      * Тест внутренней ошибки сервера.
--- react/src/main.jsx
+++ react/src/main.jsx
@@ -2,13 +2,12 @@ import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import router from './router.jsx';
 import './index.css'
-import {StrictMode} from "react";
+//import {StrictMode} from "react";
 import { QuestionProvider } from './context/QuestionContext.jsx';
 import {ContextProvider} from "./context/ContextProvider.jsx";
 import {QuizProvider} from "./context/QuizContext.jsx";
 
 createRoot(document.getElementById('root')).render(
-    <StrictMode>
         <ContextProvider>
             <QuestionProvider>
                 <QuizProvider>
@@ -16,5 +15,5 @@ createRoot(document.getElementById('root')).render(
                 </QuizProvider>
             </QuestionProvider>
         </ContextProvider>
-    </StrictMode>
+
 );
